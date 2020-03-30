8feac1fe6cae7455964eecbfe76a0cc6ded5e982
Ariel Khawly <akhawly59@gmail.com>
2020-03-30 14:27:11 -0700

    general info section in progress

--- client/src/component/pages/MedicalProfile.js
+++ client/src/component/pages/MedicalProfile.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 import medicalHistory from "../medicalChecklist";
 
 
@@ -11,32 +11,41 @@ function MedicalItem({ history }) {
 
     )
 }
-
-
-function MedicalProfile({medicalChecklist}) {
+function MedicalProfile() {
     return (
         <Fragment>
-            <h1>[Name's] Medical Information</h1>
             <form>
-                <h2>General Information</h2>
-                <input name='DOB' type='text'></input>
-                <label for='DOB'>Date of Birth</label>
-                <h2>Past Medical History</h2>
-                {medicalHistory.map((h, i) => <MedicalItem key={i} history={h} />)}
-                <h2>Health Maintence</h2>
-                <h2>Past Surgical History</h2>
-                <p>Have you ever had surgery?</p>
-                <input name='surgeryYes' type='checkbox'></input>
-                <label for='surgeryYes'>Yes</label>
-                <h2>Current Medications</h2>
-                <p>Please list all current medicines and supplements</p>
-                <h2>Allergies</h2>
+                <div>
+                    <h1>[Name's] Medical Information</h1>
+                    <h2>General Information</h2>
+                    <input name='DOB' type='text'></input>
+                    <label for='DOB'>Date of Birth</label>
+                    <p>Please select your gender assigned to you at birth:</p>
+                    <input type="radio" id="male" name="gender" value="male" checked={false}>
+                        <label for="male">Male</label>
 
+                        <input type="radio" id="female" name="gender" value="female" checked={false}>
+                            <label for="female">Female</label>
+                    </div>
+                        <div>
+                            <h2>Past Medical History</h2>
+                            {medicalHistory.map((h, i) => <MedicalItem key={i} history={h} />)}
+                            <h2>Health Maintence</h2>
+                            <h2>Past Surgical History</h2>
+                            <p>Have you ever had surgery?</p>
+                            <input name='surgeryYes' type='checkbox'></input>
+                            <label for='surgeryYes'>Yes</label>
 
-
-            </form>
-
-        </Fragment>
+                            <h2>Current Medications</h2>
+                            <p>Please list all current medicines and supplements</p>
+                            <h2>Allergies</h2>
+                            <p>Are you allergic to any medication?</p>
+                            <p>Are you allergic to latex?</p>
+                            <p>Are you allergic to any foods?</p>
+                        </div>
+                    </form>
+                    </Fragment>
+                    
     )
 }
 
