03e1801697ed6fe51e58f86161db8e3e48e2b8b0
Joel Spielberger <joelspielberger101@gmail.com>
2020-03-30 14:26:14 -0700

    added bootstrap reference in index.html

--- client/src/component/pages/LoginPage.js
+++ client/src/component/pages/LoginPage.js
@@ -4,9 +4,21 @@ function LoginPage() {
     return (
         <div>
             <h1>Log In Page</h1>
-            <p></p>
+            <input type="text"></input>
+            <form>
+                <label htmlFor="username">Username:</label>
+                <input placeholder="email" />
+                <label htmlFor="password">Password:</label>
+                <input placeholder="password" />
+                <button onClick={props.handleFormSubmit} 
+                className="btn btn-primary mt-3">Log In</button>
+            </form>
+            
         </div>
     )
 
 
 }
+//will need bcryptjs?//
+//
+export default LoginPage;
