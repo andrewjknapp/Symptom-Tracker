6f34fee006659f52ba6bdab946f135f0666272d2
Ariel Khawly <akhawly59@gmail.com>
2020-03-30 13:32:36 -0700

    initial form setup + medical history data array

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,12 @@
-import React, { Component, useEffect } from "react";
-import axios from "axios";
-import logo from "./logo.svg";
+import React, { Fragment } from "react";
 import "./App.css";
+import MedicalProfile from "./component/pages/MedicalProfile"
 
 function App() {
-  useEffect(function () {
-    axios.get('/api/users').then(({data}) => {
-      console.log(data);
-    })
-  },[]) 
-
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    <Fragment>
+    <MedicalProfile />
+    </Fragment>
   );
 }
 
--- client/src/component/pages/MedicalProfile.js
+++ client/src/component/pages/MedicalProfile.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 function MedicalProfile() {
+    return (
+        <Fragment>
+            <h1>[Name's] Medical Information</h1>
+            <form>
+                <h2>General Information</h2>
+                <input name='DOB' type='text'></input>
+                <label for='DOB'>Date of Birth</label>
+                <h2>Past Medical History</h2>
+                <input name='cancer' type='checkbox'></input>
+                <input name='anemia' type='checkbox'></input>
 
+                <label for='cancer'>Cancer</label>
+            </form>
+
+        </Fragment>
+    )
 }
 
 export default MedicalProfile;
