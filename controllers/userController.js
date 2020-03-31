30da5d4336ef19147bb4f287a489e26fddf6c391
Armadillo <ajknapp2015@gmail.com>
2020-03-31 14:23:29 -0700

    added more to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,16 @@
 const db = require("../models");
 
 module.exports = {
-    sayHello: function(req, res) {
-        res.json({hello: "hello"})
-      },
+    //Api route to add user to database. Will tailor to fit the database design we choose
     createUser: function(req, res) {
-        db.User.create({name: "Andrew"})
+        console.log(req.body);
+        const { username, password} = req.body;
+        db.User.create({username: username, password: password})
+        res.send(200);
+    },
+    // Will get all physicians that a user has favorited. now returns dummy data
+    getPhysicians: function(req, res) {
+        res.json([{name: "Dr. Billy Bob", information: { phone: "1920321234", description: "he is a good guy"}}]);
     }
+
 }
--- routes/api/user.js
+++ routes/api/user.js
@@ -5,7 +5,25 @@ const userController = require("../../controllers/userController");
 
 // Matches with /api/user/hello
 router.route("/hello")
-    .get(userController.sayHello)
     .post(userController.createUser);
+
+// Matches with /api/user/physicians
+router.route("/physicians")
+    .get(userController.getPhysicians) //find all user preferred physicians (currently returns dummy data)
+    //.post() //add new preferred physician
+    //.put() update physician information
+    //.delete() delete physician
+
+router.route("/posts")
+    //.get() find all user posts
+    //.post() add new user post
+    //.put() update post?
+    //.delete() remove post
+
+router.route("/profile")
+    //.get() return user profile information
+    //.post() // add user profile info
+    //put() update user profile info
+    //.delete() remove user profile info
 
 module.exports = router;
