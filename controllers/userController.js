f65e347d43ce62c061402396d79c21d301b3edd5
Armadillo <ajknapp2015@gmail.com>
2020-03-31 19:12:16 -0700

    Created placeholder api routes for data.

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,15 +2,48 @@ const db = require("../models");
 
 module.exports = {
     //Api route to add user to database. Will tailor to fit the database design we choose
-    createUser: function(req, res) {
-        console.log(req.body);
-        const { username, password} = req.body;
-        db.User.create({username: username, password: password})
-        res.send(200);
+    // createUser: function(req, res) {
+    //     console.log(req.body);
+    //     const { username, password} = req.body;
+    //     db.User.create({username: username, password: password})
+    //     res.json(200);
+    // },
+    getPosts: function(req, res) {
+        //return all posts by user
+        res.json("Route in progress");
+    },
+    addPost: function(req, res) {
+        //receive object of post data and add to mongo db
+        res.json("Route in progress");
+    },
+    deletePost: function(req, res) {
+        //receive id of post and delete from mongo db
+        res.json("Route in progress");
     },
     // Will get all physicians that a user has favorited. now returns dummy data
     getPhysicians: function(req, res) {
         res.json([{name: "Dr. Billy Bob", information: { phone: "1920321234", description: "he is a good guy"}}]);
+    },
+    savePhysician: function(req, res) {
+        //receive information about physician and save to mongo
+        res.json("Route in progress");
+    },
+    removePhysician: function(req, res) {
+        //receive id of physician and remove from mongo
+        res.json("Route in progress");
+    },
+    updatePhysician: function(req, res) {
+        //receive id of physician and info to be changed and update mongo
+        res.json("Route in progress");
+    },
+    getProfile: function(req, res) {
+        //receive id of user and send object of user profile
+        res.json("Route in progress");
+    },
+    setProfile: function(req, res) {
+        //receive object of userProfile and store in db
+        res.json("Route in progress");
     }
+
 
 }
--- routes/api/user.js
+++ routes/api/user.js
@@ -4,26 +4,26 @@ const userController = require("../../controllers/userController");
 //All API routes will be added here
 
 // Matches with /api/user/hello
-router.route("/hello")
-    .post(userController.createUser);
+// router.route("/hello")
+//     .post(userController.createUser);
 
 // Matches with /api/user/physicians
 router.route("/physicians")
     .get(userController.getPhysicians) //find all user preferred physicians (currently returns dummy data)
-    //.post() //add new preferred physician
-    //.put() update physician information
-    //.delete() delete physician
+    .post(userController.savePhysician) //add new preferred physician
+    .put(userController.updatePhysician) //update physician information
+    .delete(userController.removePhysician) //delete physician
 
 router.route("/posts")
-    //.get() find all user posts
-    //.post() add new user post
-    //.put() update post?
-    //.delete() remove post
+    .get(userController.getPosts) //find all user posts
+    .post(userController.addPost) //add new user post
+    //.put() //update post?
+    .delete(userController.deletePost) //remove post
 
 router.route("/profile")
-    //.get() return user profile information
-    //.post() // add user profile info
-    //put() update user profile info
-    //.delete() remove user profile info
+    .get(userController.getProfile) //return user profile information
+    .post(userController.setProfile) // add user profile info
+    //put() //update user profile info
+    //.delete() //remove user profile info
 
 module.exports = router;
