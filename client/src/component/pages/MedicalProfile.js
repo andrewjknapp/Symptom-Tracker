78131140ed2a7718223daf49561970ad89c50c0a
Ariel Khawly <akhawly59@gmail.com>
2020-03-31 19:06:11 -0700

    changed medical history to user-entered text, row adding button for medications functional

--- client/src/component/pages/MedicalProfile.js
+++ client/src/component/pages/MedicalProfile.js
@@ -1,45 +1,17 @@
 import React, { Fragment, Component, useState } from "react";
-import medicalHistory from "../medicalChecklist";
 
-// handleChange(event) {
-//     this.setState({value: event.target.value});
-//   }
-
-// value={this.state.value} onChange={this.handleChange} 
-
-
-
-function HistoryItem({ history }) {
-    return (
-        <form>
-            <input name={history.id} type='checkbox'></input>
-            <label for={history.id}> {history.name}</label>
-        </form>
-
-    )
-}
-function MedicationItem() {
-   const [med, setMed] = useState('');
-   const updateMed = (event) => {
-       setMed(event.target)
-   }
-function SurgicalItem() {
-    const [surgery, setSurgery] = useState('');
-    const updateSurgery = (event) => {
-        setSurgery(event.target)
-    }
-}
-
-
-}
 function MedicalProfile() {
+    const [medCount, setMedCount] = useState(1);
+    const updateMed = () => {
+        setMedCount(medCount + 1)
+    }
     return (
         <Fragment>
             <h1>[Name's] Medical Information</h1>
             <form>
                 <div>
                     <h2>General Information</h2>
-                    <label for='DOB'>Date of Birth</label>
+                    <label for='DOB'>Date of Birth: </label>
                     <input name='DOB' type='date'></input>
 
                     <p>Please select the sex assigned to you at birth:</p>
@@ -51,7 +23,9 @@ function MedicalProfile() {
 
                 <div>
                     <h2>Past Medical History</h2>
-                    {medicalHistory.map((h, i) => <HistoryItem key={i} history={h} />)}
+                    <p>Please list any conditions you have had, separated by a comma:</p>
+                    <textarea type='text' placeholder='Please list...'></textarea>
+                    <br></br>
 
                     <h2>Past Surgical History</h2>
                     <p>Have you ever had surgery?</p>
@@ -69,19 +43,21 @@ function MedicalProfile() {
                             <th>Dosage</th>
                             <th>How Much?</th>
                         </thead>
-                        <tr>
-                            <td><input type="input" id="medname" name="medname" /></td>
-                            <td><input type="input" id="medname" name="medname" /></td>
-                            <td><input type="input" id="medname" name="medname" /></td>
-                        </tr>
+                        {[...Array(medCount)].map((m, i) => (
+                            <tr key={i}>
+                                <td><input type="input" id="medname" name="medname" /></td>
+                                <td><input type="input" id="medname" name="dosage" /></td>
+                                <td><input type="input" id="medname" name="howmuch" /></td>
+                            </tr>
+                        ))}
                         <br></br>
-                        {/* <button id='add' onClick={() => setMed('name')}>Add</button> */}
+                        <button id='add' onClick={updateMed}>Add</button>
                     </table>
                     <h2>Allergies</h2>
                     <p>Are you allergic to any medication?</p>
                     <input id='surgery' name='surgeryYes' type='radio'></input>
-                    <label for='surgeryYes'>Yes</label>
-                    <input  placeholder='Please list...'></input>
+                    <label for='surgeryYes'>Yes </label>
+                    <input placeholder='Please list...'></input>
                     <br></br>
                     <input name='surgeryNo' type='radio'></input>
                     <label for='surgeryNo'>No</label>
@@ -95,7 +71,7 @@ function MedicalProfile() {
                     <label for='surgeryNo'>No</label>
                     <p>Are you allergic to any foods?</p>
                     <input id='surgery' name='surgeryYes' type='radio'></input>
-                    <label for='surgeryYes'>Yes</label>
+                    <label for='surgeryYes'>Yes </label>
                     <input placeholder='Please list...'></input>
                     <br></br>
                     <input name='surgeryNo' type='radio'></input>
