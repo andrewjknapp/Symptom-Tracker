bc6661141deac9fa7e7d5df7d80733d2c9e40fb4
Armadillo <ajknapp2015@gmail.com>
2020-04-01 13:45:28 -0700

    Added functionality to all necessary api routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,48 +1,102 @@
 const db = require("../models");
 
+const id = "1";
+
 module.exports = {
     //Api route to add user to database. Will tailor to fit the database design we choose
-    // createUser: function(req, res) {
-    //     console.log(req.body);
-    //     const { username, password} = req.body;
-    //     db.User.create({username: username, password: password})
-    //     res.json(200);
-    // },
-    getPosts: function(req, res) {
-        //return all posts by user
-        res.json("Route in progress");
+
+    //placeholder to create users until passport is ready
+    createUser: function(req, res) {
+        console.log(req.body);
+        const { username, password, email, firstName} = req.body;
+        db.User.create({username: username, password: password, id: "1", firstName: firstName, email: email})
+        res.json(200);
     },
+
+    getPosts: function(req, res) {
+        db.User.findOne({ id: id })
+        .then( userInfo => res.json(userInfo.posts))
+        .catch(err => res.status(422).json(err));
+        
+    },
+
     addPost: function(req, res) {
         //receive object of post data and add to mongo db
-        res.json("Route in progress");
+
+        const { title, description, symptoms } = req.body
+
+        db.User.findOneAndUpdate({ id: id }, {
+            $push: {
+                posts: { title, description, symptoms }
+            }
+        })
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
     },
+
     deletePost: function(req, res) {
         //receive id of post and delete from mongo db
-        res.json("Route in progress");
+        const { description } = req.body;
+        db.User.findOneAndUpdate({ id: id }, 
+            { $pull: { "posts": { description } } } )
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
     },
+
     // Will get all physicians that a user has favorited. now returns dummy data
     getPhysicians: function(req, res) {
-        res.json([{name: "Dr. Billy Bob", information: { phone: "1920321234", description: "he is a good guy"}}]);
+
+        db.User.findOne({ id: id })
+        .then( userInfo => res.json(userInfo.preferredPhysicians))
+        .catch(err => res.status(422).json(err));
+
     },
+
     savePhysician: function(req, res) {
         //receive information about physician and save to mongo
-        res.json("Route in progress");
+
+        const { name, information } = req.body;
+
+        db.User.findOneAndUpdate({ id: id }, { 
+            $push: { 
+                preferredPhysicians: { name, information }
+            }
+        })
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+        
     },
+
     removePhysician: function(req, res) {
         //receive id of physician and remove from mongo
-        res.json("Route in progress");
+
+        const { name } = req.body;
+        db.User.findOneAndUpdate({ id: id }, 
+            { $pull: { "preferredPhysicians": { name } } } )
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
     },
+
     updatePhysician: function(req, res) {
         //receive id of physician and info to be changed and update mongo
         res.json("Route in progress");
     },
+
     getProfile: function(req, res) {
-        //receive id of user and send object of user profile
-        res.json("Route in progress");
+        db.User.findOne({ id: id })
+        .then(dbModel => res.json(dbModel.medicalProfile))
+        .catch(err => res.status(422).json(err));
     },
+
     setProfile: function(req, res) {
-        //receive object of userProfile and store in db
-        res.json("Route in progress");
+        const { profile } = req.body;
+        db.User.findOneAndUpdate({ id: id }, {
+            $set: {
+                medicalProfile : profile
+            }
+        })
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
     }
 
 
