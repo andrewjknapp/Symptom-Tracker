e7afc8af1823310094aab819b54deb0caa50bf41
Armadillo <ajknapp2015@gmail.com>
2020-04-02 14:16:47 -0700

    Renamed user.js

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
+
 const UserSchema = new Schema({
     id: String,
 	password: { type: String, required: true },
