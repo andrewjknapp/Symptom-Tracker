223b1018f0207209217a5ca995b299f156a84dab
Ariel Khawly <akhawly59@gmail.com>
2020-04-02 11:38:33 -0700

    added widgets into component

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch, useParams } from "react-router-dom";
 import "./assets/css/Navbar.css";
-import MedicalProfile from './pages/MedicalProfile'
 
 
 function Navbar() {
--- client/src/component/pages/FindHospital.js
+++ client/src/component/pages/FindHospital.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 function FindHospital() {
     return (
-<iframe src ="http://www.ushospitalfinder.com/widgets/widget" width="400" height="200" frameborder="0"> </iframe>
+        <Fragment>
+            <iframe src="http://www.ushospitalfinder.com/widgets/widget" width="400" height="200" frameborder="0"> </iframe>
+            <iframe src="https://www.health.gov/myhealthfinder?widget=true" name="myhealthfinderframe" frameborder="0" id="myhealthfinderframe" scrolling="yes" height="550" 
+            width="100%" marginheight="0" title="myhealthfinder widget" marginwidth="0"><p>Your browser does not support iframes.</p></iframe>
+
+        </Fragment>
     )
 }
+
 export default FindHospital;
