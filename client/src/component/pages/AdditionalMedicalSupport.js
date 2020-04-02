e5342dc1ed237f2232c9733c6ebbe4f43908db4b
Ariel Khawly <akhawly59@gmail.com>
2020-04-02 12:32:16 -0700

    continuing widget

--- client/src/component/pages/AdditionalMedicalSupport.js
+++ client/src/component/pages/AdditionalMedicalSupport.js
@@ -1,7 +1,28 @@
 import React, { Fragment } from "react";
 
+
+
+let Search = React.createClass({
+    getInitialState: function() {
+        return { showResults: false };
+    },
+    onClick: function() {
+        this.setState({ showResults: true });
+    },
+    render: function() {
+        return (
+            <div>
+                <input type="submit" value="Search" onClick={this.onClick} />
+                { this.state.showResults ? <Results /> : null }
+            </div>
+        );
+    }
+});
+
 function AdditionalMedicalSupport() {
     return (
+
+
         <Fragment>
             <iframe src="http://www.ushospitalfinder.com/widgets/widget" width="400" height="200" frameborder="0"> </iframe>
             <iframe src="https://www.health.gov/myhealthfinder?widget=true" name="myhealthfinderframe" frameborder="0" id="myhealthfinderframe" scrolling="yes" height="550" 
