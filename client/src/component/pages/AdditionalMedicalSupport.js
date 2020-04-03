ba91b574dabc3af24bd9961086fa03171c893063
Ariel Khawly <akhawly59@gmail.com>
2020-04-02 19:34:06 -0700

    state set and ternary to toggle between widgets

--- client/src/component/pages/AdditionalMedicalSupport.js
+++ client/src/component/pages/AdditionalMedicalSupport.js
@@ -1,35 +1,24 @@
-import React, { Fragment, Component } from "react";
+import React, { Fragment, Component, useState } from "react";
 
 
-
-let Search = React.createClass({
-    getInitialState: function () {
-        return { showResults: false };
-    },
-    onClick: function () {
-        this.setState({ showResults: true });
-    },
-    render: function () {
-        return (
-            <div>
-                <input type="submit" value="Search" onClick={this.onClick} />
-                {this.state.showResults ? <AdditionalMedicalSupport /> : null}
-            </div>
-        );
-    }
-});
 
 function AdditionalMedicalSupport() {
+    const [hospital, setHospital] = useState(false)
+    const [service, setService] = useState(false)
     return (
 
 
         <Fragment>
-            <a href={<iframe src="http://www.ushospitalfinder.com/widgets/widget" width="400" height="200" frameborder="0"> </iframe>
-            } onClick={Search} >Hospital finder</a>
-            <iframe src="http://www.ushospitalfinder.com/widgets/widget" width="400" height="200" frameborder="0"> </iframe>
-            <iframe src="https://www.health.gov/myhealthfinder?widget=true" name="myhealthfinderframe" frameborder="0" id="myhealthfinderframe" scrolling="yes" height="550"
-                width="100%" marginheight="0" title="myhealthfinder widget" marginwidth="0"><p>Your browser does not support iframes.</p></iframe>
+            <a onClick={() => setHospital(!hospital)} >Find a Hospital</a>
+            <a onClick={() => setService(!service)}>Preventative Services</a>
+            {
+                hospital ? <iframe src="http://www.ushospitalfinder.com/widgets/widget" width="400" height="200" frameborder="0"> </iframe> : ''
+            }
 
+            {
+                service ? <iframe src="https://www.health.gov/myhealthfinder?widget=true" name="myhealthfinderframe" frameborder="0" id="myhealthfinderframe" scrolling="yes" height="550"
+                    width="100%" marginheight="0" title="myhealthfinder widget" marginwidth="0"><p>Your browser does not support iframes.</p></iframe> : ''
+            }
         </Fragment>
     )
 }
