25b3cdbbf0732676cf65784ed803052e3355b175
Ariel Khawly <akhawly59@gmail.com>
2020-04-03 11:19:52 -0700

    files

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import MedicalProfile from "./component/pages/MedicalProfile"
 import NewPost from './component/pages/NewPost/NewPost'
 import PrintOut from './component/pages/PrintOut';
 import Navbar from "./component/Navbar";
+import CreateAccountPage from "./component/pages/CreateAccountPage";
 import Chart from './component/pages/Chart';
 import AdditionalMedicalSupport from './component/pages/AdditionalMedicalSupport';
 import LandingPage from './component/pages/LandingPage'
@@ -12,24 +13,33 @@ function App() {
   return (
     <Fragment>
        <Router>
-      <Navbar />
         <Switch>
+          <Route path="/">
+            <CreateAccountPage />
+          </Route>
+
         <Route absolute path="/landing-page">
+            <Navbar />
             <LandingPage />
           </Route>
           <Route absolute path="/medical-profile">
+          <Navbar />
             <MedicalProfile />
           </Route>
           <Route absolute path="/additional-medical-support">
+            <Navbar />
             <AdditionalMedicalSupport/>
           </Route>
           <Route absolute path="/chart">
+            <Navbar />
             <Chart />
           </Route>
           <Route absolute path="/new-post">
+            <Navbar />
             <NewPost />
           </Route>
           <Route absolute path="/print-out">
+            <Navbar />
             <PrintOut />
           </Route>
         </Switch>
