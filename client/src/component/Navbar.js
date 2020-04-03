2c172d36c1cdd8a1d983f38d1fb97a7f253922dd
Ariel Khawly <akhawly59@gmail.com>
2020-04-03 13:37:55 -0700

    put navbar in all components that need it for App, redited physician component, added landing page link in nvbar

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
     return (
 
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <Link className="navbar-brand">Symptom Tracker</Link>
+            <Link className="navbar-brand" to='/landing-page'>Symptom Tracker</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
