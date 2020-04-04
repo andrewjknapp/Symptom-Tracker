c4837ad98ba54622bf6a0358f46a14b3de96a48f
Ariel Khawly <akhawly59@gmail.com>
2020-04-04 14:20:45 -0700

    navbar styling, title color and font

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -6,8 +6,8 @@ import "./assets/css/Navbar.css";
 function Navbar() {
     return (
 
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <Link className="navbar-brand" to='/landing-page'>Symptom Tracker</Link>
+        <nav className="navbar navbar-expand-lg navbar-dark navColor">
+            <Link className="navbarTitle" to='/landing-page'>Symptom Tracker</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
