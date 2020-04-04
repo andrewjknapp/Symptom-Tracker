da24d781524d50579516fdbdbb552d0a174a0fc0
Ariel Khawly <akhawly59@gmail.com>
2020-04-04 14:39:05 -0700

    Switched yes and no for better flow

--- client/src/component/pages/MedicalProfile.js
+++ client/src/component/pages/MedicalProfile.js
@@ -37,6 +37,10 @@ function MedicalProfile() {
 
                     <h2>Past Surgical History</h2>
                     <p>Have you ever had surgery?</p>
+                   
+                    <input class='medInput' id='no' name='surgery' type='radio' value='no'></input>
+                    <label class='medBody2' htmlFor='surgeryNo'>No</label>
+                    <br></br>
                     <input class='medInput'id='yes' name='surgery' type='radio' value='yes'></input>
                     <label class='medBody2' htmlFor='surgeryYes'>Yes</label>
                     <table id='table'>
@@ -57,9 +61,7 @@ function MedicalProfile() {
                         </tbody>
                     </table>
                     <button className='medButtons' type='button' id='add' onClick={updateSurgery}>Add</button>
-                    <br></br>
-                    <input class='medInput' id='no' name='surgery' type='radio' value='no'></input>
-                    <label class='medBody2' htmlFor='surgeryNo'>No</label>
+
 
                     <h2>Current Medications</h2>
                     <p>Please list all current medicines and supplements</p>
@@ -85,27 +87,32 @@ function MedicalProfile() {
                     <br></br>
                     <h2>Allergies</h2>
                     <p>Are you allergic to any medication?</p>
+                   
+                    <input class='medInput' id='no' name='surgery' type='radio' value='no' />
+                    <label class='medBody2' htmlFor='surgery'>No</label>
+                    <br></br>
                     <input class='medInput' id='yes' name='allergies' type='radio' value='yes' />
                     <label class='medBody2' htmlFor='allergies'>Yes</label>
                     <input class='medInput' placeholder='Please list...'></input>
-                    <br></br>
-                    <input class='medInput' id='no' name='surgery' type='radio' value='no' />
-                    <label class='medBody2' htmlFor='surgery'>No</label>
+                   
 
                     <p>Are you allergic to latex?</p>
-                    <input class='medInput' id='surgery' name='surgeryYes' type='radio'></input>
-                    <label class='medBody2' htmlFor='surgeryYes'>Yes</label>
-
-                    <br></br>
+                  
                     <input class='medInput' name='surgeryNo' type='radio'></input>
                     <label class='medBody2' htmlFor='surgeryNo'>No</label>
+                    <br></br>
+                    <input class='medInput' id='surgery' name='surgeryYes' type='radio'></input>
+                    <label class='medBody2' htmlFor='surgeryYes'>Yes</label>
+                
                     <p>Are you allergic to any foods?</p>
+                   
+                    <input class='medInput' name='surgeryNo' type='radio'></input>
+                    <label class='medBody2' htmlFor='surgeryNo'>No</label>
+                    <br></br>
                     <input class='medInput' id='surgery' name='surgeryYes' type='radio'></input>
                     <label class='medBody2' htmlFor='surgeryYes'>Yes </label>
                     <input class='medInput' placeholder='Please list...'></input>
-                    <br></br>
-                    <input class='medInput' name='surgeryNo' type='radio'></input>
-                    <label class='medBody2' htmlFor='surgeryNo'>No</label>
+                  
 
                 </div>
                 <div>
