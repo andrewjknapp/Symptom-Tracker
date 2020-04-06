8f822f5f8bb5a1acf73017d432b68a3052f8ddc2
Peter Kang <peeterkang@gmail.com>
2020-04-06 13:23:35 -0700

    additional commit

--- client/src/component/pages/PrintOut.js
+++ client/src/component/pages/PrintOut.js
@@ -1,8 +1,6 @@
-import React, {useState, Fragment} from "react";
+import React, { Fragment} from "react";
 import ReactToPrint from "react-to-print";
 import PrintComponent from '../PrintComponent';
-import MedicalProfile from './MedicalProfile';
-import Chart from './Chart'
 import "../assets/css/PrintOut.css";
 
 
@@ -29,44 +27,5 @@ class PrintOut extends React.Component {
         );
     }
 }
-
-
-// function PrintOut () {
-//     const [medProf, setMedProf] = useState(false)
-//     const [chart, setChart] = useState(false)
-
-//     const showMedProf = () => {
-//         setMedProf(true)
-//         setChart(false)
-//     }
-
-//     const showChart = () => {
-//         setMedProf(false)
-//         setChart(true)
-//     }
-
-//     return (
-//         <Fragment>
-//             <div className='buttonDiv' >
-//                 <a className='supportButtons glow-button' onClick={showMedProf} >View Medical Profile</a>
-//                 <a className='supportButtons glow-button' onClick={showChart}>View Chart</a>
-//             </div>
-//             <div className='prevent'>
-//                 {
-//                     medProf ?    
-//                     <MedicalProfile />
-//                      : ''
-//                 }
-//             </div>
-//             {
-//                 chart ? <Chart /> : ''
-//             }
-
-//         </Fragment>
-//     )
-
-// }
-
-
 
 export default PrintOut;
