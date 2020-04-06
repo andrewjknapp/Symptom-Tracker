dd6f4c38b11645f87bddb0e8234f528a7d4b2208
Joel Spielberger <joelspielberger101@gmail.com>
2020-04-06 14:58:32 -0700

    resolved conflicts

--- client/src/component/pages/Chart.js
+++ client/src/component/pages/Chart.js
@@ -86,106 +86,106 @@ function Chart() {
     return {
       headAcheSev, stomachAcheSev, coughSev, breathSev, labels, temperatureArr
     }
+  }
 
-    function formatDate(date) {
-      const time = new Date(date);
+  function formatDate(date) {
+    const time = new Date(date);
 
-      let dayNum = time.getDay();
-      let day;
-      switch (dayNum) {
-        case 0: day = "Sun"; break;
-        case 1: day = "Mon"; break;
-        case 2: day = "Tues"; break;
-        case 3: day = "Wed"; break;
-        case 4: day = "Thur"; break;
-        case 5: day = "Fri"; break;
-        case 6: day = "Sat"; break;
-        default: day = ""
-      }
-      let amPm = "am";
-      let hour = time.getHours();
-      if (hour > 12) {
-        hour -= 12;
-        amPm = "pm";
-      }
-
-      let minutes = time.getMinutes();
-
-      let label = `${day} ${time.getDate()}/${time.getMonth()} ${hour}:${minutes} ${amPm}`
-      return label;
+    let dayNum = time.getDay();
+    let day;
+    switch (dayNum) {
+      case 0: day = "Sun"; break;
+      case 1: day = "Mon"; break;
+      case 2: day = "Tues"; break;
+      case 3: day = "Wed"; break;
+      case 4: day = "Thur"; break;
+      case 5: day = "Fri"; break;
+      case 6: day = "Sat"; break;
+      default: day = ""
+    }
+    let amPm = "am";
+    let hour = time.getHours();
+    if (hour > 12) {
+      hour -= 12;
+      amPm = "pm";
     }
 
-    //Gets user posts from database
-    useEffect(() => {
-      API.getPosts()
-        .then(res => {
-          setPosts(res.data)
-        })
-    }, []);
+    let minutes = time.getMinutes();
 
-    //Gets the severity from the symptoms in the posts
-    useEffect(() => {
-      if (posts.length > 0) {
-
-        const { headAcheSev, stomachAcheSev, coughSev, breathSev, labels, temperatureArr } = parsePosts(posts);
-
-        const formattedLabels = labels.map(label => formatDate(label));
-        //console.log(formattedLabels);
-        let headAche = newLine("green", headAcheSev, "Headache");
-        let stomacheAche = newLine("blue", stomachAcheSev, "Stomache Ache");
-        let cough = newLine("orange", coughSev, "Cough");
-        let breath = newLine("gray", breathSev, "Shortness of Breath");
-        let temp = newLine("blue", temperatureArr, "Temperature");
-
-        let dataset;
-        if (chartToggle) {
-          dataset = [
-            headAche,
-            stomacheAche,
-            cough,
-            breath
-          ]
-        } else {
-          dataset = [
-            dataset = temp
-          ]
-        }
-
-        setChartData({
-          labels: formattedLabels,
-          datasets: dataset
-        })
-      }
-    }, [posts, chartToggle])
-
-
-    let graphLabel = chartToggle ? 'Symptom Severity' : 'Temperature';
-    let buttonLabel = chartToggle ? 'Temperature' : 'Symptom Severity';
-
-    return (
-      <div>
-        <button
-          onClick={() => setChartToggle(!chartToggle)}
-        >{buttonLabel}</button>
-        <Line
-          data={chartData}
-          options={{
-            title: {
-              display: true,
-              text: graphLabel,
-              fontFamily: 'Kanit',
-              fontColor: '#090C9B',
-              fontSize: 40
-            },
-            legend: {
-              display: true,
-              position: 'right'
-            }
-          }}
-        />
-      </div>
-    );
+    let label = `${day} ${time.getDate()}/${time.getMonth()} ${hour}:${minutes} ${amPm}`
+    return label;
   }
+
+  //Gets user posts from database
+  useEffect(() => {
+    API.getPosts()
+      .then(res => {
+        setPosts(res.data)
+      })
+  }, []);
+
+  //Gets the severity from the symptoms in the posts
+  useEffect(() => {
+    if (posts.length > 0) {
+
+      const { headAcheSev, stomachAcheSev, coughSev, breathSev, labels, temperatureArr } = parsePosts(posts);
+
+      const formattedLabels = labels.map(label => formatDate(label));
+      //console.log(formattedLabels);
+      let headAche = newLine("green", headAcheSev, "Headache");
+      let stomacheAche = newLine("blue", stomachAcheSev, "Stomache Ache");
+      let cough = newLine("orange", coughSev, "Cough");
+      let breath = newLine("gray", breathSev, "Shortness of Breath");
+      let temp = newLine("blue", temperatureArr, "Temperature");
+
+      let dataset;
+      if (chartToggle) {
+        dataset = [
+          headAche,
+          stomacheAche,
+          cough,
+          breath
+        ]
+      } else {
+        dataset = [
+          dataset = temp
+        ]
+      }
+
+      setChartData({
+        labels: formattedLabels,
+        datasets: dataset
+      })
+    }
+  }, [posts, chartToggle])
+
+
+  let graphLabel = chartToggle ? 'Symptom Severity' : 'Temperature';
+  let buttonLabel = chartToggle ? 'Temperature' : 'Symptom Severity';
+
+  return (
+    <div>
+      <button
+        onClick={() => setChartToggle(!chartToggle)}
+      >{buttonLabel}</button>
+      <Line
+        data={chartData}
+        options={{
+          title: {
+            display: true,
+            text: graphLabel,
+            fontFamily: 'Kanit',
+            fontColor: '#090C9B',
+            fontSize: 40
+          },
+          legend: {
+            display: true,
+            position: 'right'
+          }
+        }}
+      />
+    </div>
+  );
 }
 
-export default Chart
+export default Chart;
