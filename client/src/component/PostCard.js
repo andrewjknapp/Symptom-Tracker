caca2de82ce80d21f0f0b14cf410e4959bf8c182
Peter Kang <peeterkang@gmail.com>
2020-04-07 11:51:39 -0700

    post-card table added

--- client/src/component/PostCard.js
+++ client/src/component/PostCard.js
@@ -15,10 +15,25 @@ function PostCard(props) {
           <p className="card-text">
             {description}
           </p>
-          <h6 style={{fontSize: '40px'}} >Symptoms</h6>
-          <ul>
-          {symptoms.map(symp => <li style={{listStyleType: 'none'}}>{symp.type}, Severity: {symp.severity}</li>)}
-          </ul>
+
+
+          <table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">Symptom</th>
+      <th scope="col">Severity</th>
+    </tr>
+  </thead>
+  <tbody>
+    {symptoms.map(symp => 
+    <tr>
+      <td>{symp.type}</td>
+      <td>{symp.severity}</td>
+    </tr>)}
+  </tbody>
+</table>
+          
+
         </div>
         <button style={{ margin: '0 auto', width: '30%', color: 'white',backgroundColor: '#3D52D5', borderRadius:'50px'}} className="btn glow-button my-3" onClick={props.deletePost} description={description}>Delete</button>
         <div className="card-footer text-muted"></div>
