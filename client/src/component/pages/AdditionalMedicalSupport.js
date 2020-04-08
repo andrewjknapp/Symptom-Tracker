c4689de113a68c57d3dda95c95b01761555a548d
Armadillo <ajknapp2015@gmail.com>
2020-04-07 21:26:32 -0700

    Removed unused variables in AdditionalMedicalSupport.js

--- client/src/component/pages/AdditionalMedicalSupport.js
+++ client/src/component/pages/AdditionalMedicalSupport.js
@@ -3,15 +3,9 @@ import "../assets/css/AdditionalMedicalSupport.css";
 
 
 function AdditionalMedicalSupport() {
-    const [hospital, setHospital] = useState(false)
     const [service, setService] = useState(false)
 
-    const showHospital = () => {
-        setHospital(true)
-        setService(false)
-    }
     const showService = () => {
-        setHospital(false)
         setService(true)
     }
     return (
@@ -21,15 +15,9 @@ function AdditionalMedicalSupport() {
             <div className='buttonDiv' >
                 <a className='supportButtons glow-button' target="_blank" rel="noopener noreferrer" href="http://www.ushospitalfinder.com/">Find a Hospital</a>
                 <button className='supportButtons glow-button' onClick={showService}>Preventative Services</button>
-
             </div>
             <div className="buttonDiv">
                 <a className='supportButtons glow-button' target="_blank" rel="noopener noreferrer" href="https://www.cdc.gov/coronavirus/2019-ncov/index.html">CDC COVID-19 Updates</a>
-            </div>
-            <div className='prevent'>
-                {
-                    hospital ? <iframe src="http://www.ushospitalfinder.com/widgets/widget" width="400" height="200" frameBorder="0"> </iframe> : ''
-                }
             </div>
             {
                 service ? <iframe src="https://www.health.gov/myhealthfinder?widget=true" name="myhealthfinderframe" frameBorder="0" id="myhealthfinderframe" scrolling="yes" height="550"
