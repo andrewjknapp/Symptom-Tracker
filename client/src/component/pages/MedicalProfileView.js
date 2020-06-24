58987e9c5709f3a1b8348dab9c523078048de6c2
Peter Kang <peeterkang@gmail.com>
2020-06-24 14:09:30 -0700

    btns on printout component hidden

--- client/src/component/pages/MedicalProfileView.js
+++ client/src/component/pages/MedicalProfileView.js
@@ -1,116 +1,167 @@
-import React, { useState, useEffect } from "react";
-import { Redirect } from "react-router";
-import { getFromStorage } from "../../utils/storage";
-import API from "../../utils/API";
+import React, { useState, useEffect } from 'react';
+import { Redirect } from 'react-router';
+import { getFromStorage } from '../../utils/storage';
+import API from '../../utils/API';
 import '../assets/css/MedicalProfileView.css';
-import '../assets/css/loader.css'
+import '../assets/css/loader.css';
 
-function MedicalProfileView() {
+function MedicalProfileView(props) {
+  const [toEditPage, setToEditPage] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [userName, setUserName] = useState('Your');
+  const [surgeryList, setSurgeryList] = useState(null);
+  const [medicationList, setMedicationList] = useState(null);
+  const [medicalProfile, setMedicalProfile] = useState({
+    DOB: 'Decline to Answer',
+    sex: 'Prefer not to say',
+    identity: 'Prefer no to say',
+    condition: 'None',
+    conditionInfo: 'None',
+    surgery: 'None',
+    surgeryInfo: {},
+    med: 'None',
+    medicationInfo: {},
+    medAllergy: 'None',
+    latex: 'No',
+    food: 'None',
+  });
 
-    const [toEditPage, setToEditPage] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
-    const [userName, setUserName] = useState("Your");
-    const [surgeryList, setSurgeryList] = useState(null);
-    const [medicationList, setMedicationList] = useState(null);
-    const [medicalProfile, setMedicalProfile] = useState({
-        DOB: 'Decline to Answer',
-        sex: 'Prefer not to say',
-        identity: 'Prefer no to say',
-        condition: 'None',
-        conditionInfo: 'None',
-        surgery: 'None',
-        surgeryInfo: {},
-        med: 'None',
-        medicationInfo: {},
-        medAllergy: 'None',
-        latex: 'No',
-        food: 'None'
+  let {
+    DOB,
+    sex,
+    identity,
+    condition,
+    conditionInfo,
+    surgery,
+    surgeryInfo,
+    med,
+    medicationInfo,
+    medAllergy,
+    latex,
+    food,
+  } = medicalProfile;
+
+  useEffect(() => {
+    const { firstName } = getFromStorage('symptom_tracker');
+    setUserName(firstName + "'s");
+    API.getProfile().then((res) => {
+      if (res.data === '') {
+        setToEditPage(true);
+      }
+      setMedicalProfile(res.data);
+      setIsLoading(false);
     });
+  }, []);
 
-    let { DOB, sex, identity, condition, conditionInfo, surgery, surgeryInfo, med, medicationInfo, medAllergy, latex, food } = medicalProfile;
+  useEffect(() => {
+    setSurgeryList(parseSurgery(surgeryInfo));
+    setMedicationList(parseMedication(medicationInfo));
+  }, [medicalProfile]);
 
-    useEffect(()=>{
-        const { firstName } = getFromStorage('symptom_tracker');
-        setUserName(firstName + "'s");
-        API.getProfile()
-            .then(res => {
-                if(res.data === "") {
-                    setToEditPage(true);
-                } 
-                setMedicalProfile(res.data)
-                setIsLoading(false);
-            })
-    }, [])
+  const parseSurgery = (surgeryInfo) => {
+    const keys = Object.keys(surgeryInfo);
+    return keys.map((key, i) => {
+      if (key.includes('Type') === true) {
+        return <span key={i}>{surgeryInfo[key]}:</span>;
+      } else if (key.includes('Year') === true) {
+        return <p key={i}>{surgeryInfo[key]}</p>;
+      } else {
+        return '';
+      }
+    });
+  };
 
-    useEffect(()=>{
-        setSurgeryList(parseSurgery(surgeryInfo));
-        setMedicationList(parseMedication(medicationInfo));
-    },[medicalProfile])
+  const parseMedication = (medicationInfo) => {
+    const keys = Object.keys(medicationInfo);
+    return keys.map((key, i) => {
+      if (key.includes('Name') === true) {
+        return (
+          <React.Fragment key={i}>
+            <span>{medicationInfo[key]}</span>
+            <br />
+          </React.Fragment>
+        );
+      } else if (key.includes('Dosage') === true) {
+        return <span key={i}>Dosage: {medicationInfo[key]}</span>;
+      } else if (key.includes('HowMuch') === true) {
+        return <p key={i}>{medicationInfo[key]}</p>;
+      } else {
+        return '';
+      }
+    });
+  };
 
-    const parseSurgery = surgeryInfo => {
-        const keys = Object.keys(surgeryInfo);
-        return keys.map((key, i) => {
-            if (key.includes("Type") === true) {
-                return <span key={i}>{surgeryInfo[key]}:</span>
-            } else if (key.includes("Year") === true) {
-                return <p key={i}>{surgeryInfo[key]}</p>
-            } else {
-                return "";
-            }
-        })
-    }
+  return toEditPage ? (
+    <Redirect to='/medical-profile' try='hope' />
+  ) : (
+    <article>
+      <h1 className='profile-view-header'>{userName} Medical Profile</h1>
 
-    const parseMedication = medicationInfo => {
-        const keys = Object.keys(medicationInfo);
-        return keys.map((key, i) => {
-            if (key.includes("Name") === true) {
-                return (
-                    <React.Fragment key={i}>
-                        <span>{medicationInfo[key]}</span>
-                        <br/>
-                    </React.Fragment>
-                    )
-            } else if (key.includes("Dosage") === true) {
-                return <span key={i}>Dosage: {medicationInfo[key]}</span>
-            } else if (key.includes("HowMuch") === true) {
-                return <p key={i}>{medicationInfo[key]}</p>
-            } else {
-                return "";
-            }
-        })
-    }
+      {isLoading ? (
+        <h2 className='text-center loading'>Loading...</h2>
+      ) : (
+        <section>
+          <div className='text-center'>
+            {!props.isPrinting ? (
+              <button
+                className='editButton'
+                onClick={() => setToEditPage(true)}
+              >
+                Edit Profile
+              </button>
+            ) : null}
+          </div>
 
-    return toEditPage ? <Redirect to="/medical-profile" try="hope"/> :(
-        <article>
-            
-
-            <h1 className='profile-view-header'>{userName} Medical Profile</h1>
-            
-            { isLoading ? <h2 className="text-center loading">Loading...</h2> : (
-                <section>
-            <div className='text-center'> 
-            <button className='editButton' onClick={()=>setToEditPage(true)}>Edit Profile</button>
-            </div>
-
-              <div className="card profile-view-card" >
-                <ul className="profile-view-elements list-group-flush list-group">
-                    <li className="list-group-item"><span className='list-num'>1. Date of Birth:</span> {DOB}</li>
-                    <li className="list-group-item"><span className='list-num'>2. Sex:</span> {sex}</li>
-                    <li className="list-group-item"><span className='list-num'>3. Gender Identity:</span> {identity}</li>
-                    <li className="list-group-item"><span className='list-num'>4. Medical Conditions:</span> {condition}</li>
-                    {(condition === "None") ? null :<li className="list-group-item">{conditionInfo}</li>}
-                    <li className="list-group-item"><span className='list-num'>5. Surgery History:</span> {surgery}</li>
-                    {(surgery === "None") ? null : <li className="list-group-item indent">Past Surgeries: {surgeryList}</li>}
-                    <li className="list-group-item"><span className='list-num'>6. Medication:</span> {med}</li>
-                    {(med === "None") ? null : <li className="list-group-item indent">Medication List: {medicationList}</li>}
-                    <li className="list-group-item"><span className='list-num'>7. Medication Allergies:</span> {medAllergy}</li>
-                    <li className="list-group-item"><span className='list-num'>8. Latex Allergy:</span> {latex}</li>
-                    <li className="list-group-item"><span className='list-num'>9. Food Allergies:</span> {food}</li>
-                </ul>
-            </div>
-            </section>)}
-        </article>
-    )
+          <div className='card profile-view-card'>
+            <ul className='profile-view-elements list-group-flush list-group'>
+              <li className='list-group-item'>
+                <span className='list-num'>1. Date of Birth:</span> {DOB}
+              </li>
+              <li className='list-group-item'>
+                <span className='list-num'>2. Sex:</span> {sex}
+              </li>
+              <li className='list-group-item'>
+                <span className='list-num'>3. Gender Identity:</span> {identity}
+              </li>
+              <li className='list-group-item'>
+                <span className='list-num'>4. Medical Conditions:</span>{' '}
+                {condition}
+              </li>
+              {condition === 'None' ? null : (
+                <li className='list-group-item'>{conditionInfo}</li>
+              )}
+              <li className='list-group-item'>
+                <span className='list-num'>5. Surgery History:</span> {surgery}
+              </li>
+              {surgery === 'None' ? null : (
+                <li className='list-group-item indent'>
+                  Past Surgeries: {surgeryList}
+                </li>
+              )}
+              <li className='list-group-item'>
+                <span className='list-num'>6. Medication:</span> {med}
+              </li>
+              {med === 'None' ? null : (
+                <li className='list-group-item indent'>
+                  Medication List: {medicationList}
+                </li>
+              )}
+              <li className='list-group-item'>
+                <span className='list-num'>7. Medication Allergies:</span>{' '}
+                {medAllergy}
+              </li>
+              <li className='list-group-item'>
+                <span className='list-num'>8. Latex Allergy:</span> {latex}
+              </li>
+              <li className='list-group-item'>
+                <span className='list-num'>9. Food Allergies:</span> {food}
+              </li>
+            </ul>
+          </div>
+        </section>
+      )}
+    </article>
+  );
 }
 
-export default MedicalProfileView
+export default MedicalProfileView;
